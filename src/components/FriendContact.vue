fa0ac62c213ec73db9dbcac9cd6f60a1ec477f99
GriffinYang <rongxinyang1776@gmail.com>
2022-05-07 14:45:23 +0800

    master

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ visibility ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="visiblity">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visiblity: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '17764484851',
+        email: 'manuel@gmail.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.visiblity = !this.visiblity;
+    },
+  },
+};
+</script>
